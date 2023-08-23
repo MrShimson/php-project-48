9065ef71b6f7354551780a82e07d93c4ad5642d5
Семён Никифоров <semnikiforov2000@gmail.com>
2023-08-23 19:17:54 +0300

    Add new files with "generate difference" functions

---
+++ src/Cli.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DifferenceCalculator\Cli;
+
+use Docopt;
+
+use function DifferenceCalculator\Gendiff\genDiff;
+
+
+function parseArgs(object $params): array
+{
+    $params = json_decode(json_encode($params), true);
+    $args = $params['args'];
+    return $args;
+}
+
+function runUtility(): void
+{
+    $doc = <<<DOC
+    Generate diff
+
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
+    DOC;
+    
+    $params = Docopt::handle($doc);
+    
+    if ($params['--format'] === 'stylish') {
+        $pathToFile1 = $params['<firstFile>'];
+        $pathToFile2 = $params['<secondFile>'];
+        print_r(genDiff($pathToFile1, $pathToFile2));
+    }
+}
---
+++ src/Gendiff.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DifferenceCalculator\Gendiff;
+
+function getJsonContent(string $pathToFile): array
+{
+    if (!file_exists($pathToFile)) {
+        $pathToFile = __DIR__ . "/..{$pathToFile}";
+    }
+    return json_decode(file_get_contents($pathToFile), true);
+}
+
+function getKeys(array $coll1, array $coll2): array
+{
+    $keys = array_merge(array_keys($coll1), array_keys($coll2));
+    $keys = array_values(array_unique($keys));
+    sort($keys);
+    return $keys;
+}
+
+function convertTypes(array $coll): array
+{
+    $callback = function ($value) {
+        if (gettype($value) === 'boolean' || gettype($value) === 'NULL') {
+            return json_encode($value);
+        }
+        return $value;
+    };
+
+    $file = array_map(fn($value) => $callback($value), $coll);
+    return $file;
+}
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $file1 = getJsonContent($pathToFile1);
+    $file2 = getJsonContent($pathToFile2);
+
+    $file1 = convertTypes($file1);
+    $file2 = convertTypes($file2);
+
+    $keys = getKeys($file1, $file2);
+
+    $diff = array_reduce($keys, function ($acc, $key) use ($file1, $file2) {
+        if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
+            $value1 = $file1[$key];
+            $value2 = $file2[$key];
+            if ($value1 === $value2) {
+                $acc[] = "    {$key}: {$value1}";
+            } else {
+                $acc[] = "  - {$key}: {$value1}";
+                $acc[] = "  + {$key}: {$value2}";
+            }
+        } elseif (array_key_exists($key, $file1)) {
+            $value = $file1[$key];
+            $acc[] = "  - {$key}: {$value}";
+        } else {
+            $value = $file2[$key];
+            $acc[] = "  + {$key}: {$value}";
+        }
+        return $acc;
+    }, ['{']);
+    $diff[] = "}\n";
+
+    return implode("\n", $diff);
+}
