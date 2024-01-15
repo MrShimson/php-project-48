e0f2faaa2c6a84e061ab2b29d43f8a1ebbda8420
Семён Никифоров <semnikiforov2000@gmail.com>
2024-01-15 20:16:21 +0300

    Add tests and testfiles for YAML

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -8,7 +8,7 @@ use function DifferenceCalculator\Gendiff\genDiff;
 
 class GendiffTest extends TestCase
 {
-    public function testGenDiff(): void
+    public function testGenDiffWithJSON(): void
     {
         $path1 = 'file1.json';
         $path2 = 'file2.json';
@@ -22,6 +22,34 @@ class GendiffTest extends TestCase
         $this->assertEquals($expected2, genDiff($path1, $path2));
 
         $path2 = "tests/fixtures/file2.json";
+        $expected3 = <<<EOT
+        {
+          - follow: false
+            host: hexlet.io
+          - proxy: 123.234.53.22
+          - timeout: 50
+          + timeout: 20
+          + verbose: true
+        }
+        
+        EOT;
+        $this->assertEquals($expected3, genDiff($path1, $path2));
+    }
+
+    public function testGenDiffWithYAML(): void
+    {
+        $path1 = 'file1.yaml';
+        $path2 = 'file2.yaml';
+        $expected1 = ("\nThe file does not exist at this path:\n{$path1}\n");
+
+        $this->assertEquals($expected1, genDiff($path1, $path2));
+
+        $path1 = "tests/fixtures/file1.yaml";
+        $expected2 = ("\nThe file does not exist at this path:\n{$path2}\n");
+
+        $this->assertEquals($expected2, genDiff($path1, $path2));
+
+        $path2 = "tests/fixtures/file2.yaml";
         $expected3 = <<<EOT
         {
           - follow: false
